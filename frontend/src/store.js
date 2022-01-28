ec24a1c60dc03541cdc46b84b8b368d6542ced34
Carlos <thechubbscoper@gmail.com>
2022-01-28 00:36:52 -0500

    Redux

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,17 +1,21 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from "redux";
+import thunk  from "redux-thunk";
 import { products } from "./data";
-
 
 //INITIAL STATE AND REDUCER
 //RETURNS LIST OF PRODUCTS IN FRONTEND
 
-
 const initialState = {};
-const reducer = (state,action) => {
-    return {productsx: products}
+const reducer = (state, action) => {
+  return { productsx: products };
 };
 
-const store = createStore(reducer, initialState);
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
+);
 
 export default store;
-
